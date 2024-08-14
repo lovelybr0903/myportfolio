da0c29c8d0498573a0e3415b2b5b70cb90f93bcf
lovelybr0903 <lovelybr0903@gmail.com>
2024-08-14 09:45:07 +0900

    docs: 1차배포

---
+++ js/js.js
@@ -0,0 +1,38 @@
+// 탑버튼
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 250) {
+      $("#topBtn").fadeIn();
+    } else {
+      $("#topBtn").fadeOut();
+    }
+  });
+
+  $("#topBtn").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      400
+    );
+    return false;
+  });
+});
+
+// ps 이미지 변경
+function tissueImg() {
+  document.getElementById("main-img-01").src = "img/mainimg4.gif";
+}
+
+//
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  grid: {
+    rows: 2,
+  },
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
